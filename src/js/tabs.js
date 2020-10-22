640b53958c17ed70d09f6c2faec83e74bb1f4904
mixfolio <62788627+mixfolio@users.noreply.github.com>
2020-10-22 13:24:29 +0300

    upd tabs

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,56 +1,25 @@
-// // Tabs tabs.js
-// const tabs = document.querySelectorAll('.tab'),
-//    tabsContent = document.querySelectorAll('.tab-content'),
-//    tabsParent = document.querySelector('.tabs-list');
-// function hideTabContent() {
-//    tabsContent.forEach(item => {
-//       item.style.display = 'none';
-//    });
-//    tabs.forEach(tab => {
-//       tab.classList.remove('tab-active');
-//    });
-// }
-// function showTabContent(i = 0) {
-//    tabsContent[i].style.display = '';
-//    tabs[i].classList.add('tab-active');
-// }
-// hideTabContent();
-// showTabContent();
-
-// tabsParent.addEventListener('click', (event) => {
-//    const target = event.target;
-//    if (target && target.classList.contains('tab')) {
-//       tabs.forEach((item, i) => {
-//          if (target == item) {
-//             hideTabContent();
-//             showTabContent(i);
-//          }
-//       });
-//    }
-// });
-
 // Tabs tabs.js
-const tabs = document.querySelectorAll('.tab-2'),
+const tabs = document.querySelectorAll('.tab'),
    tabsContent = document.querySelectorAll('.tab-content'),
-   tabsParent = document.querySelector('.tabs-list-2');
+   tabsParent = document.querySelector('.tabs-list');
 function hideTabContent() {
    tabsContent.forEach(item => {
       item.style.display = 'none';
    });
    tabs.forEach(tab => {
-      tab.classList.remove('tab-2-active');
+      tab.classList.remove('tab--active');
    });
 }
 function showTabContent(i = 0) {
    tabsContent[i].style.display = '';
-   tabs[i].classList.add('tab-2-active');
+   tabs[i].classList.add('tab--active');
 }
 hideTabContent();
 showTabContent();
 
 tabsParent.addEventListener('click', (event) => {
    const target = event.target;
-   if (target && target.classList.contains('tab-2')) {
+   if (target && target.classList.contains('tab')) {
       tabs.forEach((item, i) => {
          if (target == item) {
             hideTabContent();
