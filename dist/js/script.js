f9660178354d2b8ae0599cf5c25c92a9c4eb9b80
mixfolio <62788627+mixfolio@users.noreply.github.com>
2021-03-29 11:40:46 +0300

    upd

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,13 +10,13 @@ btnBurger.addEventListener('click', () => {
    sidebarBtn = sidebarRight.querySelector('#sidebarBtn');
 sidebarBtn.addEventListener('click', () => {
    sidebarRight.classList.toggle('show-menu');
-   if (!sidebarRight.classList.contains('show-menu')){
-      sidebarBtn.classList.remove('icon-arr-prev');
-      sidebarBtn.classList.add('icon-arr-next');
-   } else {
-      sidebarBtn.classList.remove('icon-arr-next');
-      sidebarBtn.classList.add('icon-arr-prev');
-   }
+   // if (!sidebarRight.classList.contains('show-menu')){
+   //    sidebarBtn.classList.remove('icon-arr-prev');
+   //    sidebarBtn.classList.add('icon-arr-next');
+   // } else {
+   //    sidebarBtn.classList.remove('icon-arr-next');
+   //    sidebarBtn.classList.add('icon-arr-prev');
+   // }
    
 });
    
